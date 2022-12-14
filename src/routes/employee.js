bde7158b8e32e8f18cece4c6e39d3604346a823c
Dairo Garcia <garcianaranjodairo@gmail.com>
2022-12-14 00:59:26 -0500

    done?

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,6 +1,7 @@
 const employeeRouter = require('express').Router();
 const { Employee, Factura } = require('../db');
 const { decrypt } = require('../utils/encrypt');
+const { verifyToken } = require('../utils/middleware');
 
 employeeRouter.get('/', async (req, res) => {
   const employees = await Employee.findAll({ include: Factura });
@@ -11,6 +12,25 @@ employeeRouter.get('/:id', async (req, res) => {
   const employee = await Employee.findByPk(req.params.id);
   employee.dataValues.password = decrypt(employee.dataValues.password);
   res.json(employee);
+});
+
+employeeRouter.delete('/:id', verifyToken, async (req, res) => {
+  const { user } = req;
+
+  if (user.role !== 'ADMIN') {
+    res
+      .status(400)
+      .json({ msg: 'No tienes permiso para hacer esa transaccion' });
+  } else {
+    const employe = await Employee.findByPk(req.params.id);
+
+    if (!employe) {
+      res.status(400).json({ msg: 'Ese empleado no se encuentra registrado' });
+    } else {
+      employe.destroy();
+      res.json({ msg: 'Done!' });
+    }
+  }
 });
 
 module.exports = employeeRouter;
