7864a67cc467b0b88402194cef23929e2fdeff51
DairoGN <dair.garcia@udla.edu.co>
2023-02-07 09:30:00 -0500

    Change Report

--- src/controllers/report.js
+++ src/controllers/report.js
@@ -1,5 +1,14 @@
+const { Op } = require('sequelize');
 const {
-  Fecha, Factura, Combo, Motorcycle,
+  Fecha,
+  Factura,
+  Combo,
+  DrawOut,
+  Employee,
+  Motorcycle,
+  Person,
+  Color,
+  Brand,
 } = require('../db');
 
 module.exports = {
@@ -10,18 +19,98 @@ module.exports = {
         {
           model: Factura,
           required: false,
-          attributes: ['id', 'total', 'isPaid'],
+          attributes: ['total'],
           include: [
-            { model: Combo, required: false, attributes: ['id', 'name'] },
+            { model: Combo, required: false, attributes: ['name'] },
+            { model: Employee, required: false, attributes: ['commission'] },
+          ],
+        },
+        { model: DrawOut, required: false, attributes: ['amount'] },
+      ],
+    });
+
+    const { Facturas, DrawOuts } = data.dataValues;
+    const result = {};
+
+    result.count = Facturas.length;
+    result.total = 0;
+    result.cobroEmpleados = 0;
+    result.combos = {};
+
+    for (let i = 0; i < Facturas.length; i += 1) {
+      const element = Facturas[i];
+      result.total += Number(element.total);
+      result.combos[element.Combo.name] = !result.combos[element.Combo.name]
+        ? 1
+        : result.combos[element.Combo.name] + 1;
+
+      for (let j = 0; j < element.Employees.length; j += 1) {
+        result.cobroEmpleados
+          += (Number(element.total) * (element.Employees[j].commission / 100))
+          / element.Employees.length;
+      }
+    }
+
+    result.retiros = DrawOuts.reduce(
+      (acc, curr) => acc + Number(curr.amount),
+      0,
+    );
+
+    result.gananciaNeta = result.total - (result.cobroEmpleados + result.retiros);
+
+    return result;
+  },
+  getAllData: async () => {
+    const data = await Fecha.findAll({
+      where: {
+        date: {
+          [Op.startsWith]: ['2023/01/15'],
+        },
+      },
+      attributes: ['date'],
+      include: [
+        {
+          model: Factura,
+          required: false,
+          attributes: [
+            'total',
+            'isPaid',
+            'description',
+            'date',
+            'paymentMethod',
+          ],
+          include: [
+            {
+              model: Employee,
+              required: false,
+              attributes: [
+                'dni',
+                'names',
+                'surnames',
+                'phone',
+                'birthDate',
+                'commission',
+              ],
+            },
             {
               model: Motorcycle,
               required: false,
-              attributes: ['id', 'plaque'],
+              attributes: ['plaque'],
+              include: [
+                {
+                  model: Person,
+                  required: false,
+                  attributes: ['fullName', 'phone'],
+                },
+                { model: Color, required: false, attributes: ['color'] },
+                { model: Brand, required: false, attributes: ['brand'] },
+              ],
             },
           ],
         },
       ],
     });
+
     return data;
   },
 };
--- src/routes/report.js
+++ src/routes/report.js
@@ -1,8 +1,8 @@
 const reportRoute = require('express').Router();
-const { getDetailDate, getAllDates } = require('../controllers/report');
+const { getDetailDate, getAllData } = require('../controllers/report');
 
 reportRoute.get('/', async (req, res) => {
-  res.json(await getAllDates());
+  res.json(await getAllData('2023/01/15'));
 });
 
 reportRoute.get('/:id', async (req, res) => {
